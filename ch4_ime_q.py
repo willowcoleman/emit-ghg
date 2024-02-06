be4c1db58adc146bb0199c516ba93b95b2f6dbb2
Willow Coleman <wcoleman@g.hmc.edu>
2024-02-06 15:55:54 -0800

    update IME code

--- ch4_ime_q.py
+++ ch4_ime_q.py
@@ -35,20 +35,45 @@ from matplotlib import animation
 from PIL import Image
 from IPython.display import Image, display
 from bresenham import bresenham
+import re
 
 def main(input_args=None):
     parser = argparse.ArgumentParser(description="Methane plume IME/Q calculation")
     parser.add_argument('--plume_id', type=int, help='Methane plume ID number')
+    parser.add_argument('--out_path', type=str, default = '/beegfs/scratch/colemanr/emit-ghg/out/', nargs='?', help='Save path for output log file')
+    parser.add_argument('--out_name', type=str, default='save_data.txt', help='Name for output log file (.txt)')
     parser.add_argument('--gif', action='store_true', help="Save plume orthogonal transect gif") 
     parser.add_argument('--plot', action='store_true', help="Save IME/Q vs. plume length scatter plot")
-    parser.add_argument('--save_path', type=str, help="Save path for gif/plot outputs", nargs = '?') # Required if gif or plot
+    parser.add_argument('--plot_path', type=str, help="Save path for gif/plot outputs") # Required if gif or plot
     parser.add_argument('--grib_path', type=str, help='Save path for ERA-5 .grib file', default = '/beegfs/scratch/colemanr/emit-ghg/grib', nargs='?')
     args = parser.parse_args(input_args)
     
-    if args.save_path is None and args.gif:
-        parser.error("--gif and --save_path must be given together")
-    if args.save_path is None and args.plot:
-        parser.error("--plot and --save_path must be given together")
+    # Check for co-required arguments
+    if args.plot_path is None and args.gif:
+        parser.error("--gif and --plot_path must be given together")
+    if args.plot_path is None and args.plot:
+        parser.error("--plot and --plot_path must be given together")
+        
+    # Check that provided paths exist and create if they don't 
+    if not os.path.exists(args.out_path):
+        os.makedirs(args.out_path)
+        print("Directory '% s' created" % args.out_path) 
+    if args.plot_path is not None and not os.path.exists(args.plot_path):
+        os.makedirs(args.plot_path)
+        print("Directory '% s' created" % args.plot_path) 
+    if not os.path.exists(args.grib_path):
+        os.makedirs(args.grib_path)
+        print("Directory '% s' created" % args.grib_path) 
+    
+    # Check that args.out_name is a .txt file and if it doesn't exist yet, create it
+    if os.path.splitext(args.out_name)[1] != '.txt':
+        parser.error("--out_name must be a .txt file")
+    if not os.path.isfile(os.path.join(args.out_path, args.out_name)):
+        with open(os.path.join(args.out_path, args.out_name), 'w') as f:
+            print("File '% s' created" % os.path.join(args.out_path, args.out_name))
+            f.write('')  
+    else: 
+        print("Appending data to existing file!") 
 
     ##########################################
 
@@ -124,8 +149,25 @@ def main(input_args=None):
                 plume_lon = list(sub_df['properties.Longitude of max concentration'])[0]
                 x_source, y_source = ~ds.transform * (plume_lon, plume_lat)
 
-    u10_avg, _ = access_era5(time, day, year, month, plume_lat, plume_lon, args.grib_path)
-    q_list, ime_list = calc_q(plume_arr, full_path, u10_avg, x_source, y_source, conc_unc, args.plot, args.gif)
+    # IME/Q Calculations 
+    u10_avg, u10_std = access_era5(time, day, year, month, plume_lat, plume_lon, args.grib_path)
+    q_list, ime_list, min_radius = calc_q(plume_arr, full_path, u10_avg, u10_std, args.plume_id, x_source, y_source, conc_unc, args.plot, args.gif, args.plot_path)
+    
+    print('Threshold:', str(min_radius))
+    
+    plume_area = np.sum(plume_arr>0)
+    
+    # Write plume info to file
+    with open(os.path.join(args.out_path, args.out_name), 'a+', encoding='utf-8') as my_file:
+        my_file.write('Plume ID: ' + str(plume_id) + '\n')
+        my_file.write('Threshold: ' + str(min_radius) + '\n')
+        my_file.write('Plume Area: ' + str(plume_area) + '\n')
+        my_file.write('Max IME: ' + str(np.max(ime_list)) + '\n')
+        my_file.write('Max Q: ' + str(np.max(q_list)) + '\n')
+        my_file.write('Avg Windspeed: ' + str(u10_avg) + '\n')
+        my_file.write('Windspeed Unc: ' + str(u10_std) + '\n')
+        
+
     
 ######## FUNCTIONS ########
     
@@ -139,24 +181,29 @@ def access_era5(time, day, year, month, plume_lat, plume_lon, save_path):
     fname_ext = '.grib'
     fname = year + month + day + '_' + time + fname_ext
     fname = os.path.join(save_path, fname)
+
+    if not os.path.exists(fname): 
+        print('Downloading new .grib file!')
     
-    # Retrieve ERA5 from Climate Data Store (CDS) API 
-    c = cdsapi.Client()
-    c.retrieve(
-    'reanalysis-era5-single-levels',
-    {
-        'product_type': 'reanalysis',
-        'format': 'grib',
-        'day': day, 
-        'time': time,
-        'year': year,
-        'month': month,
-        'area': plume_coords,
-        'variable': [
-            '10m_u_component_of_wind',
-        ],
-    },
-    fname)
+        # Retrieve ERA5 from Climate Data Store (CDS) API 
+        c = cdsapi.Client()
+        c.retrieve(
+        'reanalysis-era5-single-levels',
+        {
+            'product_type': 'reanalysis',
+            'format': 'grib',
+            'day': day, 
+            'time': time,
+            'year': year,
+            'month': month,
+            'area': plume_coords,
+            'variable': [
+                '10m_u_component_of_wind',
+            ],
+        },
+        fname)
+    else:
+        print('Using pre-existing .grib file!')
     
     ds = xr.open_dataset(fname)
     df = ds.to_dataframe()
@@ -175,7 +222,7 @@ def hour_round(time):
     rounded_time = nearest_hour.strftime('%H:%M:%S')
     return rounded_time[0:-3]
 
-def calc_ime(plume_arr, plume_path, plot = False): 
+def calc_ime(plume_arr, plume_path, dst_srs, plot = False): 
     """
     plume_arr: 2-D array of plume enhancement data [ppmm]
     plume_path: filepath to plume enhancement data
@@ -184,16 +231,15 @@ def calc_ime(plume_arr, plume_path, plot = False):
     p_s: dimensions of EMIT pixel [m^2]
     """
 
-    # Get number of non-NaN pixels in a plume 
+    # Get non-NaN pixels in a plume 
     plume_only = plume_arr[plume_arr!=-9999]
-    num_pixels = plume_only.size
-
-    #      ppm(m)     L/m^3       mole/L      kg/mole
-    k = (1.0/1e6)*(1000.0/1.0)*(1.0/22.4)*(0.01604/1.0) # scaling factor from ppmm to kg CH4
-
+    
     # IME calculation 
-    p_s = pixel_size(plume_path)
-    ime = k * np.sum([mf*p_s for mf in plume_only])
+    p_s = pixel_size(plume_path, dst_srs)
+    
+    #     ppm(m)       m^2       L/m^3        mol/L      kg/mol
+    k = (1.0/1e6)*((p_s)/1.0)*(1000.0/1.0)*(1.0/22.4)*(0.01604/1.0) # scaling factor from ppmm to kg CH4
+    ime = plume_only.sum() * k 
     
     if plot: 
         plt.imshow(plume_arr)
@@ -201,22 +247,22 @@ def calc_ime(plume_arr, plume_path, plot = False):
     
     return ime, p_s
 
-def pixel_size(plume_path): 
+def pixel_size(plume_path, dst_srs): 
     """
     plume_path: filepath to plume enhancement data
     
-    p_s: dimensions of EMIT pixel [m^2]
+    p_s: area dimensions of EMIT pixel [m^2]
     """
     
-    proj_ds = gdal.Warp('', plume_path, dstSRS='EPSG:3857', format='VRT')
-    transform_3857 = proj_ds.GetGeoTransform()
-    xsize_m = transform_3857[1]
-    ysize_m = transform_3857[5]
+    proj_ds = gdal.Warp('', plume_path, dstSRS=dst_srs, format='VRT')
+    transform_ds = proj_ds.GetGeoTransform()
+    xsize_m = transform_ds[1]
+    ysize_m = transform_ds[5]
     p_s = np.abs(xsize_m * ysize_m)
     
     return p_s 
 
-def contour_plume(plume_arr, plot = True): 
+def contour_plume_UPDATE(plume_arr, plot = True): 
     """
     plume_arr: 2-D array of plume enhancement data [ppmm]
     plot: boolean, plot plume and skeleton
@@ -250,24 +296,31 @@ def contour_plume(plume_arr, plot = True):
     rows,cols = img_copy.shape[:2]
     [vx,vy,x,y] = cv2.fitLine(c, cv2.DIST_L2,0,0.01,0.01)
     left_pt = int((-x*vy/vx) + y)
-    right_pt = int(((cols-x)*vy/vx)+y)
-    curr_line = cv2.line(img_copy,(cols-1,right_pt),(0,left_pt),(0,0,255),1)
-    line_length = math.dist([cols-1,right_pt],[0,left_pt])
-
+    right_pt = int(((cols-x)*vy/vx)+y)    
+    line = ((0, left_pt), (img_copy.shape[1] - 1, right_pt))
+    
+    # Clip line to contour 
+    rect = cv2.boundingRect(c)
+    x, y, w, h = rect
+    clipped_line = cv2.clipLine(rect, line[0], line[1])
+    cv2.line(img_copy, clipped_line[1], clipped_line[2], (0, 255, 0), 1)
+    
     # Plotting
     if plot: 
         M = cv2.moments(c)
         cX = int(M["m10"]/M["m00"])
         cY = int(M["m01"]/M["m00"])
-        cv2.circle(img_copy, (cX, cY), 1, (255, 0, 0), -1)
         cv2.drawContours(image=img_copy, contours=contours, 
                          contourIdx=-1, color=(0, 255, 0), thickness=1)
+        cv2.line(img_copy, clipped_line[1], clipped_line[2], (0, 0, 255), 1)
         plt.imshow(img_copy, cmap = 'gray')
         plt.show()
         
-    right_pt = [cols-1,right_pt]
-    left_pt = [0,left_pt]
-
+    # Return calculations     
+    right_pt = clipped_line[2]
+    left_pt = clipped_line[1]
+    line_length = math.dist([clipped_line[2][0], clipped_line[2][1]], [clipped_line[1][0], clipped_line[1][1]])
+    
     return line_length, right_pt, left_pt
     
 def find_intersection(plume_arr, row_index, col_index):
@@ -280,9 +333,9 @@ def find_intersection(plume_arr, row_index, col_index):
     """
     
     # Find plume contour line and slope 
-    line_length, right_pt, left_pt = contour_plume(plume_arr, plot = False)
+    line_length, right_pt, left_pt = contour_plume_UPDATE(plume_arr, plot = False)
     slope = (right_pt[1] - left_pt[1])/(right_pt[0] - left_pt[0])
-    slope = -1/slope
+    slope = -1/(slope + 10e-6)
     
     rows, cols = plume_arr.shape
     b = row_index - slope * col_index
@@ -310,7 +363,7 @@ def find_intersection(plume_arr, row_index, col_index):
 
     return intersection_points
 
-def calc_q(plume_arr, plume_path, u10, x_source, y_source, conc_unc, plot, gif, gif_path = 'ime_q.gif'): 
+def calc_q(plume_arr, plume_path, u10, u10_std, complex_id, x_source, y_source, conc_unc, plot, gif, plot_path, dst_srs = 'EPSG:3857'): 
     """
     plume_arr [float arr]: 2-D array of plume enhancement data [ppmm]
     plume_path [str]: filepath to plume enhancement data
@@ -318,15 +371,15 @@ def calc_q(plume_arr, plume_path, u10, x_source, y_source, conc_unc, plot, gif, 
     x_source: x coordinate of plume source pixel
     y_source: y coordinate of plume source pixel
     conc_unc: scene-wise match-filter concentration uncertainty [ppmm] 
-    plot: boolean, scatterplot of IME and Q values
-    gif: boolean, gif of orthogonally transected plume 
+    plot [boolean]: scatterplot of IME and Q values
+    gif [boolean]: gif of orthogonally transected plume 
     
     q [float]: hourly plume emissions [kgCH4/hr]
     ime [float]: excess mass of CH4 in plume [kg]
     """
     
     # Get locations of plume skeleton line 
-    line_length, right_pt, left_pt = contour_plume(plume_arr, plot = False)
+    line_length, right_pt, left_pt = contour_plume_UPDATE(plume_arr, plot = False)
 
     # Rasterize line using Bresenham Line Algorithm
     indices = list(bresenham(right_pt[0], right_pt[1], left_pt[0], left_pt[1]))
@@ -350,7 +403,7 @@ def calc_q(plume_arr, plume_path, u10, x_source, y_source, conc_unc, plot, gif, 
     q_unc_list = []
     ime_unc_list = []
     frames = []
-    
+        
     if gif: 
         fig, ax = plt.subplots()
 
@@ -360,7 +413,7 @@ def calc_q(plume_arr, plume_path, u10, x_source, y_source, conc_unc, plot, gif, 
     right_distance_col = num_cols - x_source - 1
         
     # Iterate through row/col inds 
-    for i in range(0,len(row_inds), 5):
+    for i in range(0,len(row_inds), 1):
         row = row_inds[i]
         col = col_inds[i]
         
@@ -386,7 +439,7 @@ def calc_q(plume_arr, plume_path, u10, x_source, y_source, conc_unc, plot, gif, 
         curr_plume_arr[mask] = -9999 
         
         # Mask out all negative MF values 
-        curr_plume_arr = np.where(curr_plume_arr < 0, -9999, curr_plume_arr)                        
+        # curr_plume_arr = np.where(curr_plume_arr < 0, -9999, curr_plume_arr)
         
         # Only include subplumes where the source pixel is in the plume
         if curr_plume_arr[int(y_source), int(x_source)] != -9999:
@@ -397,7 +450,7 @@ def calc_q(plume_arr, plume_path, u10, x_source, y_source, conc_unc, plot, gif, 
                 frames.append([point, curr_fig])
 
             # IME/Q calculations
-            ime, p_s = calc_ime(curr_plume_arr, plume_path)
+            ime, p_s = calc_ime(curr_plume_arr, plume_path, dst_srs)
             plume_length = np.abs(math.dist([y_source, x_source],[row,col])) * np.sqrt(p_s)
             q = (ime/plume_length) * u10 * 3600 
             
@@ -405,7 +458,7 @@ def calc_q(plume_arr, plume_path, u10, x_source, y_source, conc_unc, plot, gif, 
             unc_mask = curr_plume_arr != -9999
             unc_plume_arr = curr_plume_arr.copy()
             unc_plume_arr[unc_mask] = conc_unc         
-            ime_unc, p_s = calc_ime(unc_plume_arr, plume_path)
+            ime_unc, p_s = calc_ime(unc_plume_arr, plume_path, dst_srs)
             q_unc = (ime_unc/plume_length) * u10 * 3600 
 
             q_list.append(q)
@@ -414,35 +467,80 @@ def calc_q(plume_arr, plume_path, u10, x_source, y_source, conc_unc, plot, gif, 
             ime_unc_list.append(ime_unc)
             q_unc_list.append(q_unc)
      
+    # When is scenewide uncertainty >> than measured MF in ppmm 
+    plume_threshold_list = [a / b for a, b in zip(ime_list, ime_unc_list)]
+    
+    # Find highest value in rad_list where IME/IME uncertainty > 1 
+    min_radius = 0
+    for i in range(len(plume_threshold_list)): 
+        radius = rad_list[i]
+        thresh = plume_threshold_list[i]
+        if radius > min_radius and thresh > 1: 
+            min_radius = radius
+    
+    # # Get plume complex ID 
+    # complex_id = re.search(r'-(.*?)\.tif', os.path.split(plume_path)[-1]).group(1)
+
     if gif: 
         # Display animation of concentric circles 
         ani = animation.ArtistAnimation(fig, frames, interval=500, blit=True, repeat_delay=1000)
-        ani.save(gif_path)
-        display(Image(gif_path))
+        ani.save(os.path.join(plot_path,  str(complex_id) + '_plume_ani.gif'))
+        # display(Image(plot_path))
         
     if plot: 
+        
+        plt.rcParams['font.family'] = "serif"
+        
+        # fig1, (ax1, ax3) = plt.subplots(1,2, figsize = (10, 4))
+        fig1, ax1 = plt.subplots(1,1, figsize = (5,4))
+        
+        fig1.tight_layout(pad=7)
+        
         # Plot IME/Q data
-        fig1, ax1 = plt.subplots()
-        ax1.errorbar(rad_list, q_list, yerr = q_unc_list, color = 'blue', fmt = 'o', capsize = 2)
+        ax1.scatter(rad_list, q_list, color = 'blue')
         ax1.set_xlabel('Plume length [m]')
         ax1.set_ylabel('Q [kgCH4/hr]', color = 'blue')
         ax1.tick_params(axis = 'y', labelcolor = 'blue')
         ax2 = ax1.twinx()
-        ax2.errorbar(rad_list, ime_list, yerr = ime_unc_list, color = 'r', fmt = 'o', capsize = 2)
+        ax2.scatter(rad_list, ime_list, color = 'r')
         ax2.set_ylabel('IME [kg]', color = 'r')
         ax2.tick_params(axis = 'y', labelcolor = 'r')
-        plt.show()
+        plt.savefig(os.path.join(plot_path, str(complex_id) + '_ime_q_scatter.png'), dpi=1200)
         
-        # Plot IME/Q uncertainty data
-        fig1, ax1 = plt.subplots()
-        ax1.scatter(rad_list, [a / b for a, b in zip(ime_list, ime_unc_list)], color = 'green')
-        ax1.set_xlabel('Plume length [m]')
-        ax1.set_ylabel('IME/IME Uncertainty')
-        ax1.axhline(y=1, color = 'green', linestyle = 'dashed', linewidth = 2)
-
+        ## Read in CNN bypass data 
+        cnn_df = pd.read_csv('/scratch/colemanr/emit-ghg/EMIT_plume_ime_CNN.csv')
+        
+        ime_200 = np.mean(cnn_df[cnn_df[' Candidate ID'] == ' CH4_PlumeComplex-' + str(complex_id)][' IME200 (kg)'])
+        # ax2.plot([200], [ime_200], marker='*', ls='none', color = 'r', ms=10)
+        
+        avg_ime_200 = np.mean(cnn_df[cnn_df[' Candidate ID'] == ' CH4_PlumeComplex-' + str(complex_id)][' AvgIMEdivFetch200 (kg/m)'])
+        stdev = np.mean(cnn_df[cnn_df[' Candidate ID'] == ' CH4_PlumeComplex-' + str(complex_id)][' StdIMEdivFetch200 (kg/m)'])
+        
+        ime_200 = np.mean(cnn_df[cnn_df[' Candidate ID'] == ' CH4_PlumeComplex-' + str(complex_id)][' IME200 (kg)'])
+        fetch_200 = np.mean(cnn_df[cnn_df[' Candidate ID'] == ' CH4_PlumeComplex-' + str(complex_id)][' Fetch200 (m)'])
+        
+        # Science Advances 2023
+#         ax1.axhline([(ime_200/fetch_200) * u10 * 3600], ls='solid', color = 'blue')
+#         ax1.axhline([(ime_200/fetch_200) * u10 * 3600 + (ime_200/fetch_200) * u10_std * 3600], ls='dashed', color = 'blue')
+#         ax1.axhline([(ime_200/fetch_200) * u10 * 3600 - (ime_200/fetch_200) * u10_std * 3600], ls='dashed', color = 'blue')
+        
+        # Duren Concentric Circles 2019
+        # ax1.axhline([avg_ime_200 * u10 * 3600], ls='solid', color = 'blue')
+        # ax1.axhline([avg_ime_200 * u10 * 3600 + stdev * 3600 * u10], ls='dashed', color = 'blue')
+        # ax1.axhline([avg_ime_200 * u10 * 3600 - stdev * 3600 * u10], ls='dashed', color = 'blue')
+        
+        # # Plot IME/Q uncertainty data
+        # # fig1, ax1 = plt.subplots()
+        # ax3.scatter(rad_list, plume_threshold_list, color = 'green')
+        # ax3.set_xlabel('Plume length [m]')
+        # ax3.set_ylabel('IME/IME Uncertainty')
+        # ax3.axhline(y=1, color = 'green', linestyle = 'dashed', linewidth = 2)
+        # # plt.savefig(os.path.join(plot_path, str(complex_id) + '_ime_q_scatter_unc.png'))
+        
         plt.show()
 
-    return q_list, ime_list
+
+    return q_list, ime_list, rad_list
 
     
 if __name__ == '__main__':

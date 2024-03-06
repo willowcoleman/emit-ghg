fec4279dcb5e8c0f871f0034702507aa8f65cd39
colemanr <red.willow.coleman@jpl.nasa.gov>
2024-03-06 15:07:17 -0800

    framekwork

--- ch4_ime_q.py
+++ ch4_ime_q.py
@@ -34,17 +34,21 @@ import openmeteo_requests
 import requests_cache
 from retry_requests import retry
 from openmeteo_sdk.Variable import Variable
+from LatLongUTMconversion import LLtoUTM
 
 def main(input_args=None):
     parser = argparse.ArgumentParser(description="Methane plume IME/Q calculation")
     parser.add_argument('--plume_id', type=int, help='Methane plume ID number')
-    parser.add_argument('--dst_srs', type=str, default='EPSG:3857', help="??")
     parser.add_argument('--method', type=str, default='transect', help="??")
     parser.add_argument('--plume_path', type=str, default='/scratch/brodrick/methane/visions_delivery/', help="Path to delineated plume COGs")
     parser.add_argument('--meta_path', type=str, default='/scratch/brodrick/methane/ch4_plumedir/previous_manual_annotation_oneback.json', help="Path to metadata json")
     parser.add_argument('--mask_neg', action='store_true')
     parser.add_argument('--plot', action='store_true', help="Save IME/Q vs. plume length scatter plot figure as pdf")
     parser.add_argument('--plot_path', type=str, default='/scratch/colemanr/emit-ghg/ime_2024_manuscript/transect_figs/', help="Save path for pdf plot outputs") # Required if gif or plot
+
+    parser.add_argument('--plume_lat', type=float)
+    parser.add_argument('--plume_lon', type=float)
+
     args = parser.parse_args(input_args)
 
     # Flags
@@ -64,24 +68,58 @@ def main(input_args=None):
 
     # Extract plume metadata 
     full_plume_id = 'CH4_PlumeComplex-' + str(args.plume_id)
-    u10_avg, u10_std, x_source, y_source, full_path, plume_arr, _, _, _  = get_plume_metadata(full_plume_id, args.meta_path, args.plume_path)
+    date, hour_rounded, plume_lat, plume_lon, emit_fid, full_path = get_plume_metadata_MMGIS(full_plume_id, args.meta_path, args.plume_path)
+    u10_avg, u10_std = open_meteo_era5(plume_lat, plume_lon, date, hour_rounded)
+
+    # Read plume array from .tif 
+    plume_arr, x_source, y_source = read_plume_arr(full_path, plume_lat, plume_lon)
 
     # IME/Q Calculations 
     if args.method == 'transect':
-        p_s = pixel_size(full_path, args.dst_srs)
+        p_s = pixel_size(full_path, plume_lat, plume_lon)
         q_list, ime_list, rad_list = calc_q_transect(plume_arr, p_s, u10_avg, u10_std, x_source, y_source, args.mask_neg)
 
         if args.plot_path: 
-            create_plume_pdf(full_plume_id, args.meta_path, args.plot_path, args.plume_path, q_list, ime_list, rad_list)
+            create_plume_pdf(plume_arr, x_source, y_source, rad_list, ime_list, q_list, plume_lat, plume_lon, full_plume_id, emit_fid, args.plot_path)
     else: 
         print("This method is not implemented!")
         return
 
 ######## HELPER FUNCTIONS ########
 
-def get_plume_metadata(curr_id, meta_path, plume_path):
+def read_plume_arr(full_path, plume_lat, plume_lon):
+    """
+    full_path: complete path to individual delineated plume .tif file
+    plume_lat: pseudo-origin latitude [deg]
+    plume_lon: pseudo-origin longitude [deg]
 
-    # MMGIS backend 
+    plume_arr: 2-D array of plume enhancement data [ppmm]
+    x_source: x coordinate of plume source pixel 
+    y_source: y coordinate of plume source pixel
+    """
+    with rasterio.open(full_path, 'r') as ds: 
+        plume_arr = ds.read().squeeze()
+
+    # Convert source to pixel coordinates
+    x_source, y_source = ~ds.transform * (plume_lon, plume_lat)
+
+    return plume_arr, x_source, y_source
+
+def get_plume_metadata_MMGIS(curr_id, meta_path, plume_path):
+    """
+    curr_id: plume ID string (e.g., CH4_PlumeComplex-1800)
+    meta_path: path to metadata json file scraped from MMGIS
+    plume_path: path to folder of delineated plume .tif files 
+
+    plume_lat: pseudo-origin latitude [deg]
+    plume_lon: pseudo-origin longitude [deg]
+    date: str year-month-day e.g., "2024-03-05"
+    hour_rounded: e.g., 19:00:00
+    emit_fid: EMIT acquisition FID (e.g., emit20230219t094130)
+    full_path: complete path to individual delineated plume .tif file
+    """
+
+    # MMGIS metadata 
     with open(meta_path, 'r') as f: 
         meta = json.loads(f.read())
     meta_df = pd.json_normalize(meta['features'])
@@ -98,33 +136,24 @@ def get_plume_metadata(curr_id, meta_path, plume_path):
         print(curr_id + ' missing pseudo-origin data!\n')
         return 
 
-    # Read out metadata 
-    pseudo_o  = json.loads(metadata['properties.Psuedo-Origin'])["coordinates"]
-    plume_lat = pseudo_o[1]
-    plume_lon = pseudo_o[0]
-    conc_unc = 10e-6
-    scene_fid = metadata['properties.fids']
-    date = scene_fid[0][4:8] + "-" + scene_fid[0][8:10] + "-" + scene_fid[0][10:12]
-    time_str = scene_fid[0][13:15] + ":" + scene_fid[0][15:17] + ":" + scene_fid[0][17:19]
-    hour_rounded = hour_round(time_str)
+    else: 
+        # Read out metadata 
+        pseudo_o  = json.loads(metadata['properties.Psuedo-Origin'])["coordinates"]
+        plume_lat = pseudo_o[1]
+        plume_lon = pseudo_o[0]
+        scene_fid = metadata['properties.fids']
+        date = scene_fid[0][4:8] + "-" + scene_fid[0][8:10] + "-" + scene_fid[0][10:12]
+        time_str = scene_fid[0][13:15] + ":" + scene_fid[0][15:17] + ":" + scene_fid[0][17:19]
+        hour_rounded = hour_round(time_str)
 
-    # Extract windspeed from Open-Meteo
-    u10_avg, u10_std = open_meteo_era5(plume_lat, plume_lon, date, hour_rounded)
+        # Open plume tifs, checking for plumes that may span multiple FIDs but are contained in a single plume .tif
+        for fid in scene_fid:
+            curr_folder = os.path.join(fid[4:12], 'l2bch4plm')
+            curr_file = fid + '_' + curr_id.strip() + '.tif'
+            full_path = os.path.join(plume_path, curr_folder, curr_file)
 
-    # Open plume tifs
-    for fid in scene_fid:
-        curr_folder = os.path.join(fid[4:12], 'l2bch4plm')
-        curr_file = fid + '_' + curr_id.strip() + '.tif'
-        full_path = os.path.join(plume_path, curr_folder, curr_file)
-
-        if os.path.isfile(full_path): 
-            with rasterio.open(full_path, 'r') as ds: 
-                plume_arr = ds.read().squeeze()
-
-            # Convert source to pixel coordinates
-            x_source, y_source = ~ds.transform * (plume_lon, plume_lat)
-            
-            return u10_avg, u10_std, x_source, y_source, full_path, plume_arr, fid, plume_lat, plume_lon 
+            if os.path.isfile(full_path): 
+                return date, hour_rounded, plume_lat, plume_lon, fid, full_path
 
 def open_meteo_era5(plume_lat, plume_lon, date, hour_rounded): 
     """
@@ -175,7 +204,43 @@ def open_meteo_era5(plume_lat, plume_lon, date, hour_rounded):
     
     return u10_avg, u10_std
 
+def create_plume_pdf(plume_arr, x_source, y_source, rad_list, ime_list, q_list, plume_lat, plume_lon, full_plume_id, emit_fid, plot_path):
+    """
+    Write pdf that displays plume delineation, IME/Q scatterplots 
+
+    plume_arr: 2-D array of plume enhancement data [ppmm]
+    x_source: x coordinate of plume source pixel 
+    y_source: y coordinate of plume source pixel
+    q_list: list of hourly plume emissions per transect [kgCH4/hr]
+    ime_list: list of excess mass of CH4 in plume per transect[kg]
+    rad_list: list of sub-transect fetch lengths from plume pseudo-origin [m]
+    plume_lat: pseudo-origin latitude [deg]
+    plume_lon: pseudo-origin longitude [deg]
+    emit_fid: EMIT acquisition FID (e.g., emit20230219t094130)
+    plot_path: save path for pdf plot outputs
+    """
+
+    fig, ax = plt.subplots(1,2,figsize = (10,4))
     
+    plume_img = np.where(plume_arr == -9999 , np.nan, plume_arr)
+    ax[0].imshow(plume_img, vmin = 0, vmax = 1500, cmap = 'plasma')
+    ax[0].plot(x_source, y_source, marker='o', mfc='white', color = 'black', linestyle='', ms = 7)
+    ax[1].scatter(rad_list, ime_list, color = 'red')
+    ax[1].set(xlabel = 'Plume length [m]', ylabel = 'IME [kg]')
+    ax[1].tick_params(axis = 'y', color = 'red', labelcolor = 'red')
+    ax[1].set_ylabel('IME [kg]', color = 'red')
+    ax2 = ax[1].twinx()
+    ax2.scatter(rad_list, q_list, color = 'blue')
+    ax2.tick_params(axis = 'y', color = 'blue', labelcolor = 'blue')
+    ax2.set_ylabel('Q [kgCH4/hr]', color = 'blue')
+
+    fig.tight_layout(pad=2.0)
+    fig.suptitle(full_plume_id + '\n\n' + emit_fid + '\n Pseudo-origin: (' + str(round(plume_lat,3)) + ', ' + str(round(plume_lon,3)) + ')', y=1.2, x = 0.5, ha = 'center')                    
+    fig_path = os.path.join(plot_path, full_plume_id.strip() + '_transect.pdf')
+    fig.savefig(fig_path, bbox_inches='tight')
+
+    plt.close()
+
 def hour_round(time): 
     """
     Round a time string to nearest UTC hour
@@ -205,20 +270,31 @@ def calc_ime_transect(plume_arr, p_s):
     
     return ime
 
-def pixel_size(full_path, dst_srs): 
+def pixel_size(full_path, plume_lat, plume_lon):
     """
-    full_path: filepath to plume enhancement data
-    dst_srs: EPSG code 
+    full_path: complete path to individual delineated plume .tif file
+    plume_lat: pseudo-origin latitude [deg]
+    plume_lon: pseudo-origin longitude [deg]
 
-    p_s: area dimensions of EMIT pixel [m^2]
+    p_s: dimensions of EMIT pixel [m^2]    
     """
-    proj_ds = gdal.Warp('', full_path, dstSRS=dst_srs, format='VRT')
+
+    # Extract EPSG code at pseudo-origin
+    utm_zone,_,_ = LLtoUTM(23, plume_lat, plume_lon)
+    epsg_code = 32600
+    epsg_code += int(utm_zone[:-1])
+    if plume_lat < 0:
+        epsg_code += 100
+    dstSRS = 'EPSG:' + str(epsg_code)
+
+    # Convert from deg to m 
+    proj_ds = gdal.Warp('', full_path, dstSRS=dstSRS, format='VRT')
     transform_ds = proj_ds.GetGeoTransform()
     xsize_m = transform_ds[1]
     ysize_m = transform_ds[5]
     p_s = np.abs(xsize_m * ysize_m)
-    
-    return p_s 
+
+    return p_s
 
 def contour_plume(plume_arr, plot = False): 
     """
@@ -414,30 +490,6 @@ def calc_q_transect(plume_arr, p_s, u10, u10_std, x_source, y_source, mask_neg, 
             ime_list.append(ime)            
 
     return q_list, ime_list, rad_list
-
-def create_plume_pdf(full_plume_id, meta_path, plot_path, plume_path, q_list, ime_list, rad_list):
-
-    _, _, x_source, y_source, _, plume_arr, fid, plume_lat, plume_lon  = get_plume_metadata(full_plume_id, meta_path, plume_path)
-    plume_img = np.where(plume_arr == -9999 , np.nan, plume_arr)
-                    
-    fig, ax = plt.subplots(1,2,figsize = (10,4))
-    ax[0].imshow(plume_img, vmin = 0, vmax = 1500, cmap = 'plasma')
-    ax[0].plot(x_source, y_source, marker='o', mfc='white', color = 'black', linestyle='', ms = 7)
-    ax[1].scatter(rad_list, ime_list, color = 'red')
-    ax[1].set(xlabel = 'Plume length [m]', ylabel = 'IME [kg]')
-    ax[1].tick_params(axis = 'y', color = 'red', labelcolor = 'red')
-    ax[1].set_ylabel('IME [kg]', color = 'red')
-    ax2 = ax[1].twinx()
-    ax2.scatter(rad_list, q_list, color = 'blue')
-    ax2.tick_params(axis = 'y', color = 'blue', labelcolor = 'blue')
-    ax2.set_ylabel('Q [kgCH4/hr]', color = 'blue')
-
-    fig.tight_layout(pad=2.0)
-    fig.suptitle(full_plume_id + '\n\n' + fid + '\n Pseudo-origin: (' + str(round(plume_lat,3)) + ', ' + str(round(plume_lon,3)) + ')', 
-                    y=1.2, x = 0.5, ha = 'center')                    
-    fig_path = os.path.join(plot_path, full_plume_id.strip() + '_transect.pdf')
-    fig.savefig(fig_path, bbox_inches='tight')
-    plt.close()
 
 if __name__ == '__main__':
     main()
